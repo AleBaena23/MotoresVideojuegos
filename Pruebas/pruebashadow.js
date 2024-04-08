1bd360c7627998fb66eabf77d01ff44618da9756
Mario <161206455+Maritoo00@users.noreply.github.com>
2024-04-08 17:27:26 +0000

    Mario y fernando

---
+++ Pruebas/pruebashadow.js
@@ -0,0 +1,29 @@
+import * as THREE from 'three'
+import Luisito from '/src/luisito.js'
+
+const luisito = new Luisito()
+luisito.camera.instance.position.set(2,3,7)
+
+const ambientLight = luisito.Light.CreateAmbient('white',0.5)
+
+const directionalLight = luisito.Light.CreateDirectional('white',1)
+directionalLight.position.set(5,5,1)
+
+const planeMesh = stan.mesh.CreateFromGeometry(
+    new THREE.PlaneGeometry(10,10),
+    new THREE.MeshStandardMaterial()
+)
+planeMesh.rotateX(-Math.PI/2)
+
+const cubeMesh = stan.mesh.CreateFromGeometry(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshStandardMaterial({color: 'crimson'})
+)
+cubeMesh.position.set(0,1,0)
+
+luisito.update = (dt) => {
+    cubeMesh.rotateX(dt)
+    cubeMesh.rotateY(dt)
+}
+
+luisito.Start()

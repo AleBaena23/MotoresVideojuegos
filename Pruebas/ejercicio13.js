1a1a98b98a399dc88ab30e4d7772d6dcab9cbc2a
Malfonsotejada <161016642+Malfonsotejada@users.noreply.github.com>
2024-04-28 20:55:12 +0000

    Editadas las sombras, creación de posiciones de cubos y actulizado el métoo de reset

--- Pruebas/ejercicio13.js
+++ Pruebas/ejercicio13.js
@@ -1,8 +1,14 @@
 import * as THREE from 'three'
 import * as CANNON from 'cannon-es'
 import Luisito from '/src/luisito.js'
+import CannonDebugger from 'cannon-es-debugger'
 
 const luisito = new Luisito()
+
+const cannonDebugger = new CannonDebugger(luisito.scene, luisito.world, {
+    color: 'white',
+    scale: 1
+});
 
 luisito.camera.instance.position.set(8,5,40)
 luisito.physics.world.gravity.set(0,-9.81,0)
@@ -12,17 +18,20 @@ const directionalLight = luisito.light.CreateDirectional('white', 1)
 
 // Sombras realistas
 directionalLight.position.set(10,10,10)
-directionalLight.shadow.camera.near = 0
-directionalLight.shadow.camera.far = 50
+directionalLight.shadow.camera.near = -10
+directionalLight.shadow.camera.far = 70
 directionalLight.shadow.camera.top = 40
 directionalLight.shadow.camera.right = 40
 directionalLight.shadow.camera.bottom = -40
 directionalLight.shadow.camera.left = -40
 
-const ball = luisito.createObject()
-
-// Constante de posición de la pelota 
+// Posiciones de los dos tipos de objetos que vamos a ver en la simulación
 const ball_position = new THREE.Vector3(-8, 1, 8)
+const box_position = new THREE.Vector3 (12, 1, 8)
+
+// Creamos una pelota
+const ball = luisito.createObject() 
+
 
 luisito.addComponentToObject(
     ball,
@@ -44,6 +53,76 @@ luisito.addComponentToObject(
 
 ball.position.set(ball_position.x, ball_position.y, ball_position.z)
 
+// Creamos un cubo
+const box1 = luisito.createObject()
+
+luisito.addComponentToObject(
+    box1,
+    'mesh',
+    luisito.mesh.CreateFromGeometry(
+        new THREE.BoxGeometry(2, 2, 2),
+        new THREE.MeshStandardMaterial({color: "rgb(153, 51, 255)"})
+    )
+)
+
+luisito.addComponentToObject(
+    box1,
+    'rigidbody',
+    luisito.physics.CreateBody({
+        mass: 0.1,
+        shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1))
+    })
+)
+// Posición primer cubo
+box1.position.set(box_position.x, box_position.y, box_position.z)
+
+// Creamos un segundo cubo
+const box2 = luisito.createObject()
+
+luisito.addComponentToObject(
+    box2,
+    'mesh',
+    luisito.mesh.CreateFromGeometry(
+        new THREE.BoxGeometry(2, 2, 2),
+        new THREE.MeshStandardMaterial({color: "rgb(102, 0, 255)"})
+    )
+)
+
+luisito.addComponentToObject(
+    box2,
+    'rigidbody',
+    luisito.physics.CreateBody({
+        mass: 0.25,
+        shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1))
+    })
+)
+// Posicion segundo cubo
+box2.position.set(box_position.x, box_position.y + 5, box_position.z)
+
+// Creamos un tercer cubo
+const box3 = luisito.createObject()
+
+luisito.addComponentToObject(
+    box3,
+    'mesh',
+    luisito.mesh.CreateFromGeometry(
+        new THREE.BoxGeometry(2, 2, 2),
+        new THREE.MeshStandardMaterial({color: "rgb(102, 0, 204)"})
+    )
+)
+
+luisito.addComponentToObject(
+    box3,
+    'rigidbody',
+    luisito.physics.CreateBody({
+        mass: 0.5,
+        shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1))
+    })
+)
+// Posicion segundo cubo
+box3.position.set(box_position.x, box_position.y + 9, box_position.z)
+
+// Creamos un suelo para la simulación
 const floor = luisito.createObject("floor")
 
 luisito.addComponentToObject(
@@ -70,6 +149,9 @@ floor.rotateX(-Math.PI/2)
 luisito.update = (dt) => {
     const input = luisito.input;
     const ballRigidbody = ball.rigidbody;
+    const box1Rigidbody = box1.rigidbody;
+    const box2Rigidbody = box2.rigidbody;
+    const box3Rigidbody = box3.rigidbody;
 
     // Movimiento con las teclas de dirección
     if (input.isKeyPressed('ArrowUp')) {
@@ -92,8 +174,25 @@ luisito.update = (dt) => {
 
     // Reinicio de la escena con la tecla 'r'
     if (input.isKeyPressed('r')) {
+        
+        // Reset pelota 
         ball.position.set(ball_position.x, ball_position.y, ball_position.z);
         ballRigidbody.velocity.set(0, 0, 0);
+
+        // Reset box1
+        box1.position.set(box_position.x, box_position.y, box_position.z)
+        box1.rotation.set(0, 0, 0)
+        box1Rigidbody.velocity.set(0 ,0 ,0);
+
+        // Reset box2
+        box2.position.set(box_position.x, box_position.y + 5, box_position.z)
+        box2.rotation.set(0, 0, 0)
+        box2Rigidbody.velocity.set(0 ,0 ,0)
+
+        // Reset box3
+        box3.position.set(box_position.x, box_position.y + 9, box_position.z)
+        box3.rotation.set(0, 0, 0)
+        box3Rigidbody.velocity.set(0 ,0 ,0)
     }
 
     // Colisión con el suelo

b141b4b943c032fa66deb4161808d5610e60aee7
AleBaena2306 <Ale@DESKTOP-8CGBQ6M>
2024-06-21 14:30:45 +0200

    dia5_final

--- Pruebas/pruebaPez.js
+++ Pruebas/pruebaPez.js
@@ -36,6 +36,17 @@ floor.position.set(0, 0, -10);
 
 const playerMaterial = new CANNON.Material('playerM');
 const enemyMaterial = new CANNON.Material('enemyM');
+
+
+const enemyMplayerMContactMaterial = new CANNON.ContactMaterial(
+    playerMaterial,
+    enemyMaterial,
+    {
+        friction: 0,
+        restitution: 0
+    }
+);
+luisito.physics.world.addContactMaterial(enemyMplayerMContactMaterial);
 //--------------------------------------------------------------------------------------------
 
 //MODELOS, CARGAR Y CREAR OBJETOS
@@ -143,6 +154,27 @@ let coins = []; // Array para almacenar las monedas
 let escudos = [];
 
 let rayos = [];
+
+// Esfera azul translúcida
+const geometry = new THREE.SphereGeometry(1, 32, 32);
+const material = new THREE.MeshPhongMaterial({
+    color: 0x0000ff,   // Color azul
+    transparent: true,
+    opacity: 0.3       // Opacidad para hacerla translúcida
+});
+const sphereEscudo = new THREE.Mesh(geometry, material);
+
+
+//Esfera amarilla translucida
+
+// Esfera azul translúcida
+const geometry1 = new THREE.SphereGeometry(1, 32, 32);
+const material1 = new THREE.MeshPhongMaterial({
+    color: 0xffff00,   // Color amarillo
+    transparent: true,
+    opacity: 0.3       // Opacidad para hacerla translúcida
+});
+const sphereRayo = new THREE.Mesh(geometry1, material1);
 
 luisito.onAssetsLoaded = () => {
 
@@ -283,11 +315,13 @@ const MAX_PIPE_DISTANCE = 7;
 let pulsando = false;
 const pipes = [];
 const PIPE_INTERVAL = 1.5;
-const PIPE_SPEED = 6;
+let PIPE_SPEED = 6;
 let lastPipeCenter = 0;
 var tocando = false;
 const REMOVE_INTERVAL = 7000; // Intervalo en milisegundos para eliminar las tuberías
 const REMOVE_INTERVAL_COIN = 7000
+
+const PIPE_COLLISION_RADIUS = 1.5;
 // Variables globales
 let score = 0; // Solo se declara una vez aquí
 
@@ -408,11 +442,15 @@ const createPipe = () => {
     pipeTop.rigidbody = bodyTop;
     pipeBottom.rigidbody = bodyBottom;
 
-    // Manejadores de colisión para las tuberías
+    if(vidas == 1){
+        // Manejadores de colisión para las tuberías
     pipeTop.rigidbody.addEventListener("collide", (e) => {
         if (player != null && e.body.material == playerMaterial) {
             tocando = true;
             console.log("Está tocando la tubería superior");
+            if(vidas == 2){
+                luisito.scene.remove(pipeTop)
+            }
         }
     });
 
@@ -420,8 +458,19 @@ const createPipe = () => {
         if (player != null && e.body.material == playerMaterial) {
             tocando = true;
             console.log("Está tocando la tubería inferior");
+            if(vidas == 2){
+                luisito.scene.remove(pipeBottom)
+            }
         }
     });
+    }
+
+   
+       
+    
+
+
+    
 
     // Agregar los cuerpos al mundo de Cannon.js
     world.addBody(bodyTop);
@@ -511,10 +560,24 @@ const spawnPipes = () => {
 
 const updatePipes = (dt) => {
     for (let i = pipes.length - 1; i >= 0; i--) {
+
+        
         const pipe = pipes[i];
         const bodyTop = pipe.top.rigidbody;
         const bodyBottom = pipe.bottom.rigidbody;
+        let speedIncrement = 0.025; // Incremento de velocidad por segundo
+        let elapsedTime = 0; 
+       // Incrementar el tiempo acumulado
+        elapsedTime += dt;
+        const initialIntensity = 2; // Intensidad inicial de la luz ambiental
+        const minIntensity = 0.25; // Intensidad mínima deseada
+        const decreaseRate = (initialIntensity - minIntensity) / 10; //
+            // Calcular la nueva intensidad basada en el tiempo transcurrido
+        const newIntensity = initialIntensity - decreaseRate * (elapsedTime / 1000); // 1000 para convertir dt a segundos
 
+        // Limitar la intensidad para asegurarse de que no sea menor que el mínimo
+        ambientLight.intensity = Math.max(minIntensity, newIntensity);
+        PIPE_SPEED = PIPE_SPEED + speedIncrement * elapsedTime;
         bodyTop.position.x -= PIPE_SPEED * dt;
         bodyBottom.position.x -= PIPE_SPEED * dt;
 
@@ -536,6 +599,65 @@ const updatePipes = (dt) => {
             world.removeBody(bodyBottom);
             pipes.splice(i, 1);
         }
+            // Crear cápsulas de colisión para los pipes
+            const pipeTopCapsules = [];
+            const pipeBottomCapsules = [];
+            const numCapsules = 20; // Siempre crear 10 cápsulas hacia abajo
+
+            for (let i = 0; i < numCapsules; i++) {
+                const yOffset = (i + 2) * 1.0; // Separación vertical entre cápsulas
+
+                // Posiciones de las cápsulas
+                const topPosition = new THREE.Vector3(pipe.top.position.x, pipe.top.position.y + yOffset-3, pipe.top.position.z);
+                const bottomPosition = new THREE.Vector3(pipe.bottom.position.x, pipe.bottom.position.y - yOffset +5, pipe.bottom.position.z);
+
+                // Crear cápsulas (esferas) para los pipes
+                const topCapsule = new THREE.Sphere(topPosition, PIPE_COLLISION_RADIUS);
+                const bottomCapsule = new THREE.Sphere(bottomPosition, PIPE_COLLISION_RADIUS);
+
+                pipeTopCapsules.push(topCapsule);
+                pipeBottomCapsules.push(bottomCapsule);
+            }
+
+
+                // Verificar colisiones con el jugador
+if (escudoAct && player && !rayoAct) {
+    for (let i = 0; i < pipeTopCapsules.length; i++) {
+        const topCapsule = pipeTopCapsules[i];
+        const bottomCapsule = pipeBottomCapsules[i];
+
+        if (playerCollisionSphere.intersectsSphere(topCapsule)) {
+            luisito.scene.remove(pipe.top);
+            world.removeBody(bodyTop);
+            tocando = true;
+            break; // Salir del bucle si hay colisión
+        } else if (playerCollisionSphere.intersectsSphere(bottomCapsule)) {
+            luisito.scene.remove(pipe.bottom);
+            world.removeBody(bodyBottom);
+            tocando = true;
+            break; // Salir del bucle si hay colisión
+        }
+    }
+}
+
+if (rayoAct && player) {
+    for (let i = 0; i < pipeTopCapsules.length; i++) {
+        const topCapsule = pipeTopCapsules[i];
+        const bottomCapsule = pipeBottomCapsules[i];
+
+        if (playerCollisionSphere.intersectsSphere(topCapsule)) {
+            luisito.scene.remove(pipe.top);
+            world.removeBody(bodyTop);
+            break; // Salir del bucle si hay colisión
+        } else if (playerCollisionSphere.intersectsSphere(bottomCapsule)) {
+            luisito.scene.remove(pipe.bottom);
+            world.removeBody(bodyBottom);
+            break; // Salir del bucle si hay colisión
+        }
+    }
+}
+
+
         // Verificar si el jugador pasa por el centro del tubo
         if (!pipe.scored && player.position.x > pipe.top.position.x) {
             pipe.scored = true; // Marcamos como puntuado para evitar sumar más de una vez
@@ -582,40 +704,49 @@ const checkPositionLimits = () => {
 
 var vidas = 1;
 player.position.z = 0
+var escudoAct = false;
+var rayoAct = false
+
+let tiempoCompletado = false; // Variable inicial
+
+// Función para cambiar la variable a false después de 3 segundos
+function cambiarVariable() {
+    setTimeout(() => {
+        tiempoCompletado = true;
+        console.log("Variable cambiada a false después de 3 segundos");
+    }, 3000); // 3000 milisegundos = 3 segundos
+}
+
+const PLAYER_COLLISION_RADIUS = 1.25; // Radio de la esfera de colisión del jugador
+const COIN_COLLISION_RADIUS = 0.5; // Radio de la esfera de colisión de las monedas
+const ESCUDO_COLLISION_RADIUS = 1; // Radio de la esfera de colisión de las monedas
+const RAYO_COLLISION_RADIUS = 1; // Radio de la esfera de colisión de las monedas
+
+// Crear una esfera de colisión para el jugador
+const playerCollisionSphere = new THREE.Sphere(player.position, PLAYER_COLLISION_RADIUS);
+
+
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 
 //METODO UPDATE
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
-
 luisito.update = (dt) => {
     const input = luisito.input;
 
-    // if(coin){
-    //     coin.position.x -= PIPE_SPEED * dt;
-    //     coin.rotateY(-dt);
-    // }
-
-    // if(escudo){
-    //     escudo.position.x -= PIPE_SPEED * dt;
-    //     escudo.rotateY(-dt);
-    // }
-
-    // if(rayo){
-    //     rayo.position.x -= PIPE_SPEED * dt;
-    //     rayo.rotateY(-dt);
-    // }
 
 
 
     coins.forEach(coin => {
         coin.position.x -= PIPE_SPEED * dt;
         coin.rotateY(-dt);
+         // Crear una esfera de colisión para la moneda
+         const coinCollisionSphere = new THREE.Sphere(coin.position, COIN_COLLISION_RADIUS);
 
         if(coin){
             // Verificar si el jugador está tocando la moneda basándose en la posición
-        if (player.position.distanceTo(coin.position) < 1.25 && !coin.collected) { // Ajusta el valor según sea necesario
+        if (playerCollisionSphere.intersectsSphere(coinCollisionSphere) && !coin.collected) { // Ajusta el valor según sea necesario
            
     
             // Marcar la moneda como recogida
@@ -643,19 +774,19 @@ luisito.update = (dt) => {
     escudos.forEach(escudo => {
         escudo.position.x -= PIPE_SPEED * dt;
         escudo.rotateY(-dt);
-
-        if(escudo){
+        const escudoCollisionSphere = new THREE.Sphere(escudo.position, ESCUDO_COLLISION_RADIUS);
+        if(rayoAct == false){if(escudo){
             // Verificar si el jugador está tocando la moneda basándose en la posición
-        if (player.position.distanceTo(escudo.position) < 2 && !coin.collected) { // Ajusta el valor según sea necesario
+        if (playerCollisionSphere.intersectsSphere(escudoCollisionSphere)&& !escudo.collected) { // Ajusta el valor según sea necesario
            
-    
+            
             // Marcar la moneda como recogida
-            coin.collected = true;
+            escudo.collected = true;
     
-
+            player.add(sphereEscudo)
             // Reproducir un sonido de feedback
             sonido_moneda.play();
-    
+            escudoAct = true
             // Eliminar la moneda de la escena
             luisito.scene.remove(escudo);
             escudos.splice(index, 1);
@@ -663,7 +794,29 @@ luisito.update = (dt) => {
         setTimeout(() => {
             removeCoin(escudo);
         }, REMOVE_INTERVAL_COIN);
-        }
+        }}
+        else{
+            if(escudo){
+                // Verificar si el jugador está tocando la moneda basándose en la posición
+            if (playerCollisionSphere.intersectsSphere(escudoCollisionSphere)&& !escudo.collected) { // Ajusta el valor según sea necesario
+               
+                
+                // Marcar la moneda como recogida
+                escudo.collected = true;
+                // Reproducir un sonido de feedback
+                sonido_moneda.play();
+
+                // Eliminar la moneda de la escena
+                luisito.scene.remove(escudo);
+                escudos.splice(index, 1);
+            }          
+            setTimeout(() => {
+                removeCoin(escudo);
+            }, REMOVE_INTERVAL_COIN);
+            }}
+
+        
+        
         
     });
 
@@ -672,17 +825,27 @@ luisito.update = (dt) => {
     rayos.forEach(rayo => {
         rayo.position.x -= PIPE_SPEED * dt;
         rayo.rotateY(-dt);
+        const rayoCollisionSphere = new THREE.Sphere(rayo.position, RAYO_COLLISION_RADIUS);
 
         if(rayo){
             // Verificar si el jugador está tocando la moneda basándose en la posición
-        if (player.position.distanceTo(rayo.position) < 2 && !coin.collected) { // Ajusta el valor según sea necesario
+        if (playerCollisionSphere.intersectsSphere(rayoCollisionSphere)&& !rayo.collected) { // Ajusta el valor según sea necesario
            
     
             // Marcar la moneda como recogida
-            coin.collected = true;
-
+            rayo.collected = true;
+            player.add(sphereRayo)
+            rayoAct = true;
+                    // Programar la eliminación de la tubería después de REMOVE_INTERVAL milisegundos
+            setTimeout(() => {
+                tiempoCompletado = true;
+            }, 10000);
             // Reproducir un sonido de feedback
             sonido_moneda.play();
+            if(escudoAct == true){
+                escudoAct = false
+                player.remove(sphereEscudo)
+            }
     
             // Eliminar la moneda de la escena
             luisito.scene.remove(rayo);
@@ -733,17 +896,39 @@ luisito.update = (dt) => {
             checkPositionLimits();
         }
     }
-    if(player != null && tocando == true){
-        if (vidas >0){
-            vidas -= 1;
-            tocando = false;
+    if(player != null){
+
+        if (rayoAct==true){
+            if(tiempoCompletado){
+                rayoAct = false;
+                player.remove(sphereRayo)
+            }
+        }            
+        else{
+            if (escudoAct && tocando == true){
+                tocando = false;
+                escudoAct = false;
+                player.remove(sphereEscudo)
+            }
+    
+            else if (tocando == true){
+                if (vidas == 1){
+                    vidas -= 1;
+                    tocando = false;
+                }
+            }
         }
+        
+       
+
+
         
         if(vidas == 0){
             world.removeBody(player);
             luisito.scene.remove(player);
             player = null;
             sonido_fondo.stop();
+            sonido_moneda.stop();
             sonido_perder.play();
             // alert("Has muerto");
             // window.location.reload();
@@ -756,6 +941,10 @@ luisito.update = (dt) => {
         
 
     updatePipes(dt);
+    if(tiempoPasado < 10000){
+        tiempoPasado += dt
+    }
+    
 };
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
@@ -765,12 +954,12 @@ luisito.update = (dt) => {
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 
-
+let tiempoPasado =0;
 setTimeout(() => {
     spawnPipes(); // Pregunta, ¿las tuberías despawnean?
     // Coloca aquí el código que quieres ejecutar después del retraso
     console.log("Se ha ejecutado después del retraso.");
-}, 1000); // 1000 milisegundos = 1 segundo
+}, 1000-tiempoPasado * 0.025); // 1000 milisegundos = 1 segundo
 
 
 // Obtener el modal y los elementos relacionados
